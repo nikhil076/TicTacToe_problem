3ee86706a89dbc61dc938a10355a61f91648a0fd
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-25 09:40:31 +0530

    refactored the code and added the 3*3 matrix

--- src/com/bridgelabz/tictactoe_problem/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe_problem/TicTacToeGame.java
@@ -9,11 +9,20 @@ public class TicTacToeGame {
 	
 	public static void initializeBoard() 
 	{
-		char[] Board = new char[9];
-			for(int i =0;i<Board.length;i++)
+		char[] Board = new char[10];
+			for(int i =1;i<Board.length;i++)
 			{
+				if(i%3!=0)
+				{
 					Board[i]='-';
-					System.out.print(Board[i]);	
+					System.out.print(Board[i]);
+				}
+				else
+				{
+					Board[i]='-';
+					System.out.print(Board[i]);
+					System.out.println();
+				}
 			}
   	}
 }
