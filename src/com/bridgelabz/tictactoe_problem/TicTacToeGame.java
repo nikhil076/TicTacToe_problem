09003bd1f32bc48b6428e3be24e03a156009d32c
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-26 22:45:01 +0530

    Refactored the method to select starting player and assigning values to it

--- src/com/bridgelabz/tictactoe_problem/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe_problem/TicTacToeGame.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.tictactoe_problem;
 
+import java.util.Random;
 import java.util.Scanner;
-
 
 public class TicTacToeGame
 {
+	static boolean playerStarts;
 	static char[] board = new char[10];
+	public static int line;
 	public static void main(String[] args)
 	{
 		initializeBoard();
@@ -14,13 +16,52 @@ public class TicTacToeGame
 		char computerLetter =(userLetter =='X') ? 'O' : 'X';
 		
 		System.out.println("User Letter is :"+userLetter+" \nComputer Letter is :"+computerLetter);
-		printBoard();
 		tossToStartFirst();
-		int user_index = indexSelection();
-		checkFreeSpace(user_index,userLetter,computerLetter);
+		printBoard();
+		if(playerStarts==true) 
+		{
+			playerStartsFirst(computerLetter,userLetter);
+		}
+		else
+		{
+			computerStartsFirst(computerLetter,userLetter);
+		}
+			
 		
 	}
 	
+	private static void computerStartsFirst(char computerValue, char userValue) {
+		for(int i =1;i<board.length;i++)
+		{
+			if(i%2==0)
+			{
+				int user_index = indexSelection(true);
+				checkFreeSpace(user_index, userValue);
+			}
+			else {
+				checkFreeSpaceComputer(computerValue);
+			}
+			
+		}
+	}
+
+	private static void playerStartsFirst(char computerValue, char userValue) {
+		for(int i =1;i<board.length;i++)
+		{
+			if(i%2==0)
+			{
+				checkFreeSpaceComputer(computerValue);
+			}
+			else {
+				
+				int user_index = indexSelection(true);
+				checkFreeSpace(user_index, userValue);
+			}
+			
+		}
+		
+	}
+
 	public static void initializeBoard() 
 	{
 	char[] board = new char[10];
@@ -60,25 +101,47 @@ public class TicTacToeGame
                            + " |");
         System.out.println("|---|---|---|");
 	}
-	public static int indexSelection() 
+	public static int indexSelection(boolean spaceCheck) 
 	{
+		int user_index_value;
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the index value between 1 to 9");
-		int user_index_value = scanner.nextInt();
+		if(spaceCheck == true)
+		{
+			System.out.println("Enter the index value between 1 to 9");
+			user_index_value = scanner.nextInt();
+		}
+		else
+		{
+			System.out.println("Index value is not empty");
+			user_index_value = scanner.nextInt();
+		}
 		return user_index_value;
 	}
-	public static void checkFreeSpace(int user_selection, char Player_value ,char computer_value)
+	public static void checkFreeSpace(int user_selection, char player_value )
 	{
 		if(board[user_selection]==0)
 		{
 			System.out.println(user_selection+" index is free");
-			board[user_selection] = Player_value;
+			board[user_selection] = player_value;
 			printBoard();
 		}
 		else
 		{
 			System.out.println(user_selection+" index is not free");
-			checkFreeSpace(user_selection, Player_value, computer_value);
+			indexSelection(false);
+		}
+	}
+	public static void checkFreeSpaceComputer(char computer_value)
+	{
+		int random = (int) (Math.floor(Math.random()*10) % 8)+1;
+		if(board[random]==0)
+		{
+			board[random]=computer_value;
+			printBoard();
+		}
+		else
+		{
+			checkFreeSpaceComputer(computer_value);
 		}
 	}
 	public static void tossToStartFirst()
@@ -90,10 +153,12 @@ public class TicTacToeGame
 		double headOrTails = Math.floor(Math.random() * 10) % 2;
 		if(headOrTails==player_choice)
 		{
+			playerStarts = true;
 			System.out.println("player starts first ");
 		}
 		else
 		{
+			playerStarts = false;
 			System.out.println("Computer starts first ");
 		}
 	}
